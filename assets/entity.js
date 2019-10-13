53ea9d9fe0321fbf5e331f98d77f50b5aa29d3c5
artanis <artanisace@gmail.com>
2019-10-14 01:49:38 +0200

    added mixing groups

--- assets/entity.js
+++ assets/entity.js
@@ -1,14 +1,17 @@
 Game.Entity = function(properties) {
   properties = properties || {};
-  // Call the glyph's construtor with our set of properties
+  // Call the glyph's constructor with our set of properties
   Game.Glyph.call(this, properties);
   // Instantiate any properties from the passed object
   this._name = properties["name"] || "";
   this._x = properties["x"] || 0;
   this._y = properties["y"] || 0;
+  this._map = null;
   // Create an object which will keep track what mixins we have
   // attached to this entity based on the name property
   this._attachedMixins = {};
+  // Create a similar object for groups
+  this._attachedMixinGroups = {};
   // Setup the object's mixins
   const mixins = properties["mixins"] || [];
   for (let i = 0; i < mixins.length; i++) {
@@ -17,16 +20,21 @@ Game.Entity = function(properties) {
     // also make sure not to override a property that
     // already exists on the entity.
     for (let key in mixins[i]) {
-      if (
-        key != "init" &&
-        key != "name" &&
-        !Object.prototype.hasOwnProperty.call(this, key)
-      ) {
+      for (let key in mixins[i]) {
+        if (
+          key != "init" &&
+          key != "name" &&
+          !Object.prototype.hasOwnProperty.call(this, key)
+        ) {
         this[key] = mixins[i][key];
       }
     }
     // Add the name of this mixin to our attached mixins
     this._attachedMixins[mixins[i].name] = true;
+    // If a group name is present, add it
+    if (mixins[i].groupName) {
+      this._attachedMixinGroups[mixins[i].groupName] = true;
+    }
     // Finally call the init function if there is one
     if (mixins[i].init) {
       mixins[i].init.call(this, properties);
@@ -55,12 +63,34 @@ Game.Entity.prototype.getX = function() {
 Game.Entity.prototype.getY = function() {
   return this._y;
 };
+Game.Entity.prototype.setMap = function(map) {
+  this._map = map;
+};
+Game.Entity.prototype.getMap = function() {
+  return this._map;
+};
 
 Game.Entity.prototype.hasMixin = function(obj) {
-  // Allow passing the mixin itself or the name as a string
-  if (typeof obj === "object") {
-    return this._attachedMixins[obj.name];
+  // Allow passing the mixin itself or the name / group name as a string
+  if (typeof obj === 'object') {
+      return this._attachedMixins[obj.name];
   } else {
-    return this._attachedMixins[name];
+      return this._attachedMixins[obj] || this._attachedMixinGroups[obj];
   }
+}
+
+const PlayerMoveableMixin = {
+  name: "PlayerMoveable",
+  groupName: "Moveable",
+  tryMove: function(x, y) {}
+};
+const MoleratMoveableMixin = {
+  name: "MoleratMoveable",
+  groupName: "Moveable",
+  tryMove: function(x, y) {}
+};
+const GhostMoveableMixin = {
+  name: "GhostMoveable",
+  groupName: "Moveable",
+  tryMove: function(x, y) {}
 };
